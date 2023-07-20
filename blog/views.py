88c27390a19444a3ac16fb7a008b8431e062fad2
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2023-07-20 11:30:48 -0300

    Updates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'autor': 'Marcelo',
+        'titulo': 'Primeiro post',
+        'conteudo': 'Conteudo do primeiro post',
+        'data': '20 de Julho, 2023'
+    },
+    {
+        'autor': 'Gustavo',
+        'titulo': 'Segundo post',
+        'conteudo': 'Conteudo do segundo post',
+        'data': '21 de Julho, 2023'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    contexto = {
+        'posts': posts
+
+    }
+    return render(request, 'blog/home.html', contexto)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
