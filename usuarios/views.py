6181c3d8afb06250c2b4929f5553761dc7cb201e
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2023-08-10 11:07:23 -0300

    Pendência de Login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import Registro
 
 def register(request):
@@ -13,3 +14,7 @@ def register(request):
     else:
         form = Registro()
     return render(request, 'usuarios/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'usuarios/profile.html')
